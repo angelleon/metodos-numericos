d3fd4cfa7b3ec480c392cd8db0822c2c01710096
Angel Leon <luianglenlop@gmail.com>
2017-02-22 17:46:00 -0600

    "versión 0.9"

--- biseccion.py
+++ biseccion.py
@@ -3,46 +3,167 @@
 import sys
 import re
 import time
+import numpy
+import pylab
 
 
-def evaluar_poly(x, exponentes=[0], coeficientes=[1]):
-	print("funcion evaluar_poly")
-	#print(x, exponentes, coeficientes)
+class Numero(enumerate):
+	flotante = 1
+	entero = 2
+
+
+class ErrorEntrada(Exception):
+	pass
+
+
+class ErrorCoeficiente(ErrorEntrada):
+	pass
+
+
+class FuncionConstante(ErrorEntrada):
+	pass
+
+
+class CaracterInvalido(ErrorEntrada):
+	pass
+
+
+class FuncionMultivariable(ErrorEntrada):
+	pass
+
+
+def convertir_a_numero(raw_numero):
+	pass
+
+
+def convertir_fraccion(fraccion):
+	print("funcion convertir fracciones")
+	motor = re.compile("[-+]?[0-9]+|[-+]?[0-9]\.[0-9]+")
+	numerador = motor.search(fraccion)
+	fraccion = fraccion[:numerador.start()] + fraccion[numerador.end():]
+	numerador = float(numerador.group())
+	denominador = motor.search(fraccion)
+	denominador = float(denominador.group())
+	return numerador/denominador
+
+
+def evaluar_poly(x, coeficientes, exponentes):
 	imagen = 0
 	for i in range(0, len(coeficientes)):
-		imagen += coeficientes[i] * x ** exponentes[0]
+		imagen += coeficientes[i] * x ** exponentes[i]
 	return imagen
+
+
+def introducir_poly():
+	while True:
+		raw_poly = input("""Introduzca el polinomio en la forma:
+		anx^n + ... + a2x^2 + a1x + a0\npolinomio: """)
+		try:
+			analizar_poly(raw_poly)
+		except ErrorEntrada:
+			continue
+		break
+
+
+def introducir_extremos():
+	while True:
+		try:
+			x0 = float(input("Introduzca el extremo izquierdo del intervalo\nx0: "))
+		except ValueError:
+			print("introduzca un valor numerico")
+			continue
+		break
+	while True:
+		try:
+			xf = float(input("Introduzca el extremo derecho del intervalo\nxf: "))
+		except ValueError:
+			continue
+		if xf == x0:
+			print("x0 y xf deben ser diferentes")
+			continue
+		break
+	return [x0, xf]
+
+
+def introducir_parametros():
+	print("funcion introducir_poly")
+	introducir_poly()
+
+
+def introducir_poly_dummy():
+	pass
 
 
 def modo_interactivo():
 	print("funcion modo_interactivo")
-	print("Opciones")
-	return [None, None, None, None]
+	while True:
+		respuesta = input("""Opciones:
+	1. Modo normal
+	2. Modo dummy""")
+		if respuesta == '1':
+			return introducir_parametros()
+		elif respuesta == '1':
+			return introducir_poly_dummy()
+		else:
+			print("Opción incorrecta")
+
+
+def buscr_carac_invalidos(raw_poly):
+	motor = re.compile("[^a-z0-9-\+^\./]")
+	if motor.search(raw_poly) is not None:
+		mostrar_error(motor.search(raw_poly).group(), raw_poly)
+		raise CaracterInvalido
+
+
+def obtener_terminos_x(raw_poly):
+	terminos = []
+	termino = ''
+	motor = re.compile("([+-]?([0-9]*|[0-9]*\.[0-9]+)[a-z](\^[0-9])?)")
+	while termino is not None:
+		termino = motor.search(raw_poly)
+		if termino is not None:
+			terminos.append(termino.group())
+		else: break
+		raw_poly = raw_poly[0:termino.start()] + raw_poly[termino.end():]
+	return [terminos, raw_poly]
+
+
+def reducir_terminos_lineales(terminos):
+	termino_lineal = 0
+	for i in terminos:
+		i = float(i)
+		termino_lineal += i
+	return str(termino_lineal)
+
+
+def obtener_terminos_lineales(raw_poly):
+	print("funcion obtener_terminos_lineales")
+	print(raw_poly)
+	motor = re.compile("([-+]?[0-9]+)|([-+]([0-9]*\.[0-9]+))")
+	terminos = []
+	termino = motor.search(raw_poly)
+	while termino is not None and len(raw_poly) != 0:
+		termino = motor.search(raw_poly)
+		print(termino.group())
+		if termino is not None:
+			terminos.append(termino.group())
+			break
+		raw_poly = raw_poly[0:termino.start()] + raw_poly[termino.end():]
+	print(terminos)
+	termino = reducir_terminos_lineales(terminos)
+	return termino
 
 
 def obtener_terminos(raw_poly):
 	print("funcion obtener_terminos")
-	motor = re.compile("[^a-z0-9-\+^\.]")
-	error = motor.search(raw_poly)
-	if error is not None:
-		print("error en el polinomio\nno se comprende:", error.group())
-		return error.group()
-	motor = re.compile("([+-]?([0-9]*|[0-9]*\.[0-9]+)[a-z](\^[0-9])?)")
-	terminos = []
-	while True:
-		print("longitud: ", len(raw_poly), "\npolinomio: " + str(raw_poly))
-		print(terminos)
-		termino = motor.search(raw_poly)
-		if len(raw_poly) == 0:
-			break
-		elif termino is not None:
-			terminos.append(termino.group())
-		elif termino is None and len(raw_poly) > 0:
-			print("Fin de los terminos con x")
-			motor = re.compile("[+-]?([0-9]*|[0-9]*\.[0-9]+)+")
-			continue
-		raw_poly = raw_poly[0:termino.start()] + raw_poly[termino.end():]
-	print("terminos: ", terminos)
+	buscr_carac_invalidos(raw_poly)
+	respuesta = obtener_terminos_x(raw_poly)
+	terminos = respuesta[0]
+	raw_poly = respuesta[1]
+	print(terminos, raw_poly)
+	terminos.append(obtener_terminos_lineales(raw_poly))
+	print(terminos)
+	#raise Exception
 	return terminos
 
 
@@ -59,16 +180,18 @@ def mostrar_error(error, raw_poly):
 
 def obtener_variable(terminos):
 	print("funcion obtener_variable")
+	print(terminos)
 	motor = re.compile("[a-z]")
 	variables = set()
 	for i in terminos:
+		print(type(i))
 		coincidencia = motor.search(i)
 		if coincidencia is not None:
 			variables.add(coincidencia.group())
 			variable = coincidencia.group()
 	if len(variables) > 1:
 		print("Ingrese funciones de una variable")
-		return None
+		raise FuncionMultivariable
 	elif len(variables) == 0:
 		print("El método de bisección no se puede aplicar a funciones constantes")
 		return None
@@ -183,7 +306,7 @@ def ordenar(coeficientes, exponentes, terminos):
 	return [coef_ordenados, exponentes, term_ordenados]
 
 
-def analizar_poly(raw_poly=''):
+def analizar_poly(raw_poly):
 	print("funcion analizar_poly")
 	if len(raw_poly) == 0:
 		print("No hay entrada")
@@ -214,19 +337,18 @@ def analizar_poly(raw_poly=''):
 	for i in terminos:
 		polinomio += str(i)
 	print("polinomio: ", polinomio)
-	time.sleep(10)
 	return [coeficientes, exponentes, polinomio, variable]
 
 
 def probar_intervalo(coeficientes, exponentes, x0, xf):
 	print("funcion probar_intervalo")
-	if evaluar_poly(x0, exponentes, coeficientes) * evaluar_poly(xf, exponentes, coeficientes) < 0:
+	if evaluar_poly(x0, coeficientes, exponentes) * evaluar_poly(xf, coeficientes, exponentes) < 0:
 		return [x0, xf]
 	dx = abs(x0 - xf) / float(100)
 	xi = x0
 	for i in range(0, 100):
 		print(xi, dx, x0, xf)
-		if evaluar_poly(xi, exponentes, coeficientes) * evaluar_poly(xf, exponentes, coeficientes) < 0:
+		if evaluar_poly(xi, coeficientes, exponentes) * evaluar_poly(xf, coeficientes, exponentes) < 0:
 			if i <= 50:
 				return [x0, xi]
 			else:
@@ -234,6 +356,13 @@ def probar_intervalo(coeficientes, exponentes, x0, xf):
 		xi += dx
 
 	return False
+
+"""
+Escribir un programa que lea un polinomio de grado n>1 e implemente el metodo de newton para aproximar una raiz
+graficar el polinomio y las rectas
+
+
+"""
 
 
 def bisectar(coeficientes, exponentes, x0, xf, n_signif):
@@ -244,9 +373,13 @@ def bisectar(coeficientes, exponentes, x0, xf, n_signif):
 	xm = (x0 + xf) / float(2)
 	xm_anterior = xm * 2
 	while abs(error) > tolerancia:
-		if evaluar_poly(x0, exponentes, coeficientes) * evaluar_poly(xm, exponentes, coeficientes) < 0:
+		if xm == 0:
+			xm = (x0 + xm)/2.0 + (x0 + xf) / 100.0
+		if evaluar_poly(xm, coeficientes, exponentes) == 0:
+			break
+		elif evaluar_poly(x0, coeficientes, exponentes) * evaluar_poly(xm, coeficientes, exponentes) < 0:
 			xf = xm
-		elif evaluar_poly(xm, exponentes, coeficientes) * evaluar_poly(xf, exponentes, coeficientes) < 0:
+		elif evaluar_poly(xm, coeficientes, exponentes) * evaluar_poly(xf, coeficientes, exponentes) < 0:
 			x0 = xm
 		error = (xm - xm_anterior) * 100.0 / xm
 		xm_anterior = xm
@@ -272,35 +405,38 @@ def imprimir_ayuda():
 
 def main(argv):
 	print("funcion main")
-	if len(argv) == 1:
-		parametros = modo_interactivo()
-	elif len(argv) == 2 and (argv[1] == "-h" or argv[1] == "--help"):
-		imprimir_ayuda()
-	elif len(argv) != 5:
-		print("El programa recible 4 argumentos, %d dados" % (len(argv) - 1))
-		imprimir_ayuda()
-		return
-	elif len(argv) == 5:
-		parametros = analizar_poly(argv[1])
-		if parametros is not None:
-			try:
-				parametros.append(float(argv[2]))
-			except ValueError:
-				print("X0 debe ser un número")
-				return
-			try:
-				parametros.append(float(argv[3]))
-			except ValueError:
-				print("Xf debe ser un número")
-				return
-			try:
-				parametros.append(int(argv[4]))
-			except ValueError:
-				print("CIFRAS_SIGNIFICATIVAS debe ser un número")
-				return
-		else:
-			print("Error en el polinomio")
+	try:
+		if len(argv) == 1:
+			parametros = modo_interactivo()
+		elif len(argv) == 2 and (argv[1] == "-h" or argv[1] == "--help"):
+			imprimir_ayuda()
+		elif len(argv) != 5:
+			print("El programa recible 4 argumentos, %d dados" % (len(argv) - 1))
+			imprimir_ayuda()
 			return
+		elif len(argv) == 5:
+			parametros = analizar_poly(argv[1])
+			if parametros is not None:
+				try:
+					parametros.append(float(argv[2]))
+				except ValueError:
+					print("X0 debe ser un número")
+					return
+				try:
+					parametros.append(float(argv[3]))
+				except ValueError:
+					print("Xf debe ser un número")
+					return
+				try:
+					parametros.append(int(argv[4]))
+				except ValueError:
+					print("CIFRAS_SIGNIFICATIVAS debe ser un número")
+					return
+			else:
+				print("Error en el polinomio")
+				return
+	except (ErrorCoeficiente, ErrorEntrada, CaracterInvalido):
+		pass
 	coeficientes = parametros[0]
 	exponentes = parametros[1]
 	polinomio = parametros[2]
@@ -326,13 +462,14 @@ def main(argv):
 	resultados = bisectar(coeficientes, exponentes, x0, xf, n_signif)
 	tiempo_ejecucion = time.clock() - inicio
 	raiz = resultados[0]
-	iteraciones = resultados[2]
-	error = resultados[3]
+	iteraciones = resultados[1]
+	error = resultados[2]
 	print("Resultados:")
-	print("Raiz encontrada en " + variable + "= " + raiz)
-	print("Iteraciones: " + iteraciones)
-	print("Error: " + error)
+	print("Raiz encontrada en ", variable, "= ", raiz)
+	print("Iteraciones: ", iteraciones)
+	print("Error: ", error)
 	print("Tiempo de ejecución: ", tiempo_ejecucion)
+	input("presione enter para graficar la función")
 
 if __name__ == '__main__':
 	main(sys.argv)
