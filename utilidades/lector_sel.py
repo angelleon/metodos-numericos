d25e3c331b784a2d60dfc276bd96fcf57fb6b7ae
Angel Leon <luianglenlop@gmail.com>
2018-01-05 16:40:52 -0600

    :u

--- utilidades/lector_sel.py
+++ utilidades/lector_sel.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
+
+
 """
 lector_sel.py
 
@@ -20,6 +22,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA 02110-1301, USA.
 """
+
+__doc__ = """Modulo que define funciones para leer matrices interactivamente
+y convertir el texto ingresado en objetos. Este archivo por si mismo no hace nada"""
 
 import logging
 from .fracciones import *
@@ -45,7 +50,7 @@ class ErrorSintactico(Exception):
         return "Error sintactico col: " + str(self.col)
 
 
-class FinMatriz(Exception):
+class FinSEL(Exception):
     pass
 
 
@@ -72,23 +77,25 @@ class Token:
     def __repr__(self):
         return self.__str__()
 
-    def __str_clasif(self):
-        if self.clasificacion == 0:
+    def str_clasif(self, clasificacion):
+        if clasificacion == 0:
             return "desconocido"
-        elif self.clasificacion == 1:
+        elif clasificacion == 1:
             return "signo"
-        elif self.clasificacion == 2:
+        elif clasificacion == 2:
             return "entero"
-        elif self.clasificacion == 3:
+        elif clasificacion == 3:
             return "decimal"
-        elif self.clasificacion == 4:
+        elif clasificacion == 4:
             return "incognita"
-        elif self.clasificacion == 5:
+        elif clasificacion == 5:
             return "cociente"
-        elif self.clasificacion == 6:
+        elif clasificacion == 6:
             return "igual"
-        elif self.clasificacion == 7:
+        elif clasificacion == 7:
             return "fin S.E.L."
+        else:
+            return "desconocido"
 
 
 class AnalizadorLexico(Token):
@@ -96,7 +103,6 @@ class AnalizadorLexico(Token):
         super().__init__()
         if len(raw_ecu) == 0:
             self.vacio = True
-            raise EntradaVacia
         else:
             self.vacio = False
         self.__raw_ecu = raw_ecu
@@ -108,12 +114,22 @@ class AnalizadorLexico(Token):
     def set_attrib(self, attrib: dict):
         self.__pos_i = attrib["pos"]
 
+    def set_raw_ecu(self, raw_ecu: str):
+        self.__raw_ecu = raw_ecu
+        self.__pos_i = 0
+        if len(raw_ecu) == 0:
+            self.vacio = True
+            raise EntradaVacia
+        else:
+            self.vacio = False
+
     def next_token(self):
         log.debug("next token()")
+        log.debug("pos_i: " + str(self.__pos_i))
         f = -1
         estado = 0
         buffer = ""
-        raw_ecu = self.__raw_ecu[self.__pos_i]
+        raw_ecu = self.__raw_ecu[self.__pos_i:]
         log.debug(raw_ecu)
         while estado != f:
             if len(raw_ecu) > 0:
@@ -170,7 +186,8 @@ class AnalizadorLexico(Token):
             elif estado in (1, 7, 9):
                 estado = f
             elif estado == 10:
-                raise FinMatriz
+                self.clasificacion = self.fin_sel
+                estado = f
             if estado > 0:
                 buffer += c
             if estado != f:
@@ -179,20 +196,22 @@ class AnalizadorLexico(Token):
             if self.__pos_i == len(self.__raw_ecu):
                 self.vacio = True
         self.contenido = buffer
+        log.debug("clasif: {0}: {3}, cont: \"{1}\", pos_i: {2}".format(self.clasificacion, self.contenido, self.__pos_i, self.str_clasif(self.clasificacion)))
 
 
 class AnalizadorSintactico(AnalizadorLexico):
     def __init__(self, raw_ecu: str):
         super().__init__(raw_ecu)
-        if len(raw_ecu) == 0:
-            raise EntradaVacia
 
     def match(self, clasificacion: int):
+        log.debug("match({0}: {1})".format(clasificacion, self.str_clasif(clasificacion)))
         attrib = self.get_attrib()
         self.next_token()
         if self.clasificacion != clasificacion:
             self.set_attrib(attrib)
+            log.debug("Not found: {0}: {1}".format(clasificacion, self.str_clasif(clasificacion)))
             return
+        log.debug("Found: {0}: {1}".format(clasificacion, self.str_clasif(clasificacion)))
         if clasificacion == self.entero:
             return int(self.contenido)
         if clasificacion == self.decimal:
@@ -202,27 +221,45 @@ class AnalizadorSintactico(AnalizadorLexico):
                 return self.positivo
             else:
                 return self.negativo
+        if clasificacion == self.incognita:
+            return self.contenido
+        else:
+            return True
 
     def ecuacion(self):
-        self.miembro()
+        log.debug("ecuacion()")
+        prim_miem = self.miembro()
         coincidencia = self.match(self.igual)
         if coincidencia is None:
             raise ErrorSintactico(self.get_attrib()["pos"])
-        self.miembro()
+        seg_miem = self.miembro()
+        fin_sel = self.match(self.fin_sel)
+        return ((prim_miem, seg_miem), fin_sel)
 
     def miembro(self):
+        log.debug("miembro()")
         coincidencia = self.termino()
         if coincidencia is None:
             raise ErrorSintactico(self.get_attrib()["pos"])
-        terminos = [coincidencia]
+        terminos = coincidencia
         coincidencia = self.rest_terminos()
+        rest_term = self.expandir_term(coincidencia)
+        for key, value in rest_term.items():
+            if key in terminos.keys():
+                terminos[key] += value
+            else:
+                terminos[key] = value
+        log.debug(str(type(terminos)) + str(terminos) + str(type(rest_term)) + str(rest_term))
+        return terminos
 
-    def expandir_term(self, rest_term: tuple[dict]):
+    @staticmethod
+    def expandir_term(rest_term: tuple):
         """
         :parameter self: object
-        :param rest_term: tuple(dict)
-        :rtype: object
+        :param rest_term: iterable
+        :rtype: dict
         """
+        log.debug("expandir_term()")
         term = {}
         while True:
             for key in rest_term[0].keys():
@@ -235,6 +272,7 @@ class AnalizadorSintactico(AnalizadorLexico):
             rest_term = rest_term[1]
 
     def termino(self):
+        log.debug("termino()")
         coincidencia = self.match(self.signo)
         if coincidencia is None:
             signo = self.positivo
@@ -257,6 +295,10 @@ class AnalizadorSintactico(AnalizadorLexico):
         return {variable: signo * coeficiente}
 
     def rest_terminos(self):
+        """
+        :rtype: tuple
+        """
+        log.debug("rest_term()")
         coincidencia = self.match(self.signo)
         if coincidencia is None:
             return {"const": 0}, None
@@ -279,6 +321,7 @@ class AnalizadorSintactico(AnalizadorLexico):
         return {variable: signo * coef}, self.rest_terminos()
 
     def variable(self):
+        log.debug("variable()")
         attrib = self.get_attrib()
         coincidencia = self.match(self.incognita)
         if coincidencia is None:
@@ -293,6 +336,7 @@ class AnalizadorSintactico(AnalizadorLexico):
             return str(variable) + str(coincidencia)
 
     def numero(self):
+        log.debug("numero()")
         attrib = self.get_attrib()
         coincidenca = self.fraccion()
         if coincidenca is None:
@@ -316,6 +360,7 @@ class AnalizadorSintactico(AnalizadorLexico):
         return entero + decimal
 
     def fraccion(self):
+        log.debug("fraccion()")
         attrib = self.get_attrib()
         coincidencia = self.match(self.entero)
         if coincidencia is None:
@@ -339,166 +384,53 @@ class AnalizadorSintactico(AnalizadorLexico):
 
 class LectorSEL:
     def __init__(self, cuadrado=False, fracciones=True):
+        self.an_sint = AnalizadorSintactico("")
         self.cuadrada = cuadrado
         self.fracciones = fracciones
         self.matriz_coef = Matriz()
         self.matriz_indep = Matriz()
 
-    def reducir(self, prim_miem, seg_miem):
-        const = 0
-        incog = {}
-        for dicc in prim_miem:  # recorriendo lista de diccionarios
-            if 'const' in dicc.keys():
-                const += (-1) * dicc['const']  # "moviendo" costantes al segundo miembro
-            else:
-                for key in dicc.keys():  # selecciona la única clave del diccionario
-                    if key not in incog.keys():  # buscando incognitas repetidas
-                        incog[key] = dicc[key]  # agrega la incognita al diccionario de incognitas
-                    else:  # cuando la incognita ya se encontro anteriormente en el primer miembro
-                        incog[key] += dicc[key]  # el valor de los coeficientes
-                        #  se suma para así reducir terminos semejantes
-        prim_miem = []
-        for key, value in incog.keys(), incog.values():
-            prim_miem.append({key: value})  # el primer miembro ahora solo tiene incognitas no repetidas
-        seg_miem.append({'const': const})
-        const = 0
-        incog = {}
-        for dicc in seg_miem:
-            if 'const' in dicc.keys():
-                const += dicc['const']  # reduce las constantes del segundo miembro
-            else:
-                for key in dicc.keys():
-                    if key not in incog.keys():
-                        incog[key] = (-1) * dicc[key]  # "moviendo" incognitas al primer miembro
-                    else:
-                        incog[key] += (-1) * dicc[key]  # reduciendo incognitas del segundo miembro
-                        # y "moviendolas" al primero
-        seg_miem = const
-        for dicc in prim_miem:
-            for key in incog.keys():
-                if key in dicc.keys():
-                    dicc[key] += incog[key]  # reduciendo terminos semejantes en el primer miembro
-                    # despues de "moverlos" desde el segundo
-        incog = {}
-        for dicc in prim_miem:
-            for key in dicc:
-                incog[key] = dicc[key]
-        prim_miem = incog
-        return prim_miem, seg_miem
+    def corregir(self):
+        pass
 
-    def ordenar(self, prim_miem):
-        longitudes = {}
-        for key in prim_miem.keys():  # obtiene las incognitas
-            if str(len(key)) in longitudes.keys():  # obtiene la longitud del string que representa cada una
-                longitudes[str(len(key))] = [key]  # crea una lista para las strings de longitud n
-            else:
-                longitudes[str(len(key))].append(key)  # todas las incognitas de longitud n van a una lista
-        elementos = []
-        for longitud in sorted(longitudes.keys()):  # se ordenan las longitudes
-            for key in sorted(longitudes[longitud]):  # se ordenan las incognitas de longitud n
-                elementos.append({key: prim_miem[key]})  # se agregan las incognitas en orden con sus coeficientes
-        return elementos
-
-    def convertir(self, raw_ecu):
-        an_lex = AnalizadorLexico(raw_ecu)
-        elementos = []
+    def leer_ecu(self, n_ecu):
+        raw_ecu = ''
         while True:
-            if an_lex.vacio:
-                break
-            elementos.append(an_lex.next_token())
-        i = 0
-        an_sint = AnalizadorSintactico(elementos)
-        elementos = []
-        while True:
-            elementos.append(an_sint.next_elemento())
-            if len(an_sint.tokens) == 0:
-                break
-        pos = len(elementos)
-        for i in range(len(elementos)):
-            if 'igual' in elementos[i].keys():
-                pos = i
-                break
-        if pos in (0, len(elementos)):
-            raise ErrorSintactico
-        prim_miem = elementos[0:pos]
-        seg_miem = elementos[pos+1:]
-        prim_miem, indep = self.reducir(prim_miem, seg_miem)
-        prim_miem = self.ordenar(prim_miem)
-        coef = []
-        incog = []
-        for dicc in prim_miem:
-            for key in dicc:
-                incog.append(key)
-                coef.append(dicc[key])
-        return [coef, incog, indep]
-
-    def leer_ecu(self, n_ecu=1):
-        raw_ecu = None
-        while True:
-            raw_ecu = input("Ingrese ecuación {0}\n(E{0}): ".format(n_ecu))
+            raw_ecu = input("Ingrese ecuación\n(E{0}): ".format(n_ecu + 1))
+            self.an_sint.set_raw_ecu(raw_ecu)
             try:
-                ecu = self.convertir(raw_ecu)
-            except ErrorLexico as e_lex:
-                print(e_lex)
+                return self.an_sint.ecuacion()
+            except (ErrorLexico, ErrorSintactico) as error:
+                print(error)
                 continue
-            except ErrorSintactico as e_sint:
-                print(e_sint)
-                continue
-            break
-        return ecu
-
-    def corregir(self, sel):
-        while True:
-            print(sel)
-            answ = input("""¿Desea corregir algo?
-    <Enter>\tTodo correcto
-    2\tCorregir renglón
-    3\tIngresar matriz de nuevo
-    (opción): """)
-            if answ == "1":
-                n_ecu = None
-                while True:
-                    answ = input("¿Qué ecuación desea corregir?\n(n): ")
-                    try:
-                        n_ecu = int(answ)
-                    except ValueError:
-                        continue
-                    except KeyboardInterrupt:
-                        break
-                    sel[n_ecu] = self.leer_ecu(n_ecu)
-                    break
-            break
-        return sel, False
 
     def leer_sel(self):
-        i = 0
-        j = -1
         sel = SEL()
-        coef = None
-        indep = None
-        incog = None
+        ecuaciones = []
+        n_ecu = 0
         while True:
-            while True and i != j:
-                if i > 0:
-                    print(sel)
-                try:
-                    sel.append(self.leer_ecu(i + 1))
-                except FinMatriz:
-                    break
-                if i == 0 and self.cuadrada:
-                    j = len(matriz_coef[0])
-                i += 1
-            try:
-                bandera = True
-                while bandera:
-                    matriz_coef, bandera = self.corregir(matriz_coef)
-            except FinMatriz:
+            ecu, fin_sel = self.leer_ecu(n_ecu)
+            ecu = self.reducir(ecu)
+            if fin_sel:
                 break
-            if not bandera:
-                break
-        return matriz_coef
+            ecuaciones.append(ecu)
+            n_ecu += 1
+        return 0
+
+    @staticmethod
+    def reducir(ecu):
+        prim_miem = ecu[0]
+        seg_miem = ecu[1]
+        for key, value in seg_miem.items():
+            if key != "const":
+                if key in prim_miem.keys():
+                    prim_miem[key] -= value
+                    del seg_miem[key]
+        if "const" in prim_miem.keys():
+            seg_miem["const"] -= prim_miem["const"]
+            del prim_miem["const"]
+        return prim_miem, seg_miem
 
 
 if __name__ == '__main__':
-    print("""Modulo que define funciones para leer matrices interactivamente
-y convertir el texto ingresado en objetos Fraccion. Este archivo por si mismo no hace nada""")
+    print(__doc__)
